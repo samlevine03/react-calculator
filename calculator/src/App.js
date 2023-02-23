79d5f6be2bd4274dfecfb774c2139aa0be984a8b
Sam Levine <samuellevine@cs.uchicago.edu>
2023-02-23 00:04:10 -0600

    made the number buttons put a number in the input. also made the clear button clear it.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,34 +2,66 @@ import React, { useState } from 'react'
 import './App.css';
 
 const App = () => {
-    const [res, setRes] = useState("");
+    const [input, setInput] = useState('');
+    const [operator, setOperator] = useState('');
+
+    const handleNumber = (e) => {
+        const value = e.target.innerText;
+        setInput(prevInput => prevInput + value);
+    }
+
+    const handleClear = () => {
+        setInput('');
+    }
+
+    const handleOperator = (e) => {
+        const value = e.target.innerText;
+        setOperator(value);
+    }
 
     return (
         <>
             <div className = "container">
-                <form>
-                    <input type="text" value={res} />
-                </form>
+                <input type="text" value={input} className="input-text"/>
 
                 <div className="keypad">
-                    <button className="toprow">AC</button>
-                    <button className="toprow">+/-</button>
+                    <button className="toprow" onClick={handleClear}>AC</button>
+
+                    <button className="toprow" onClick={handleOperator}>+/-</button>
+
                     <button className="toprow">%</button>
+
                     <button className="operator">&divide;</button>
-                    <button className="number">7</button>
-                    <button className="number">8</button>
-                    <button className="number">9</button>
-                    <button className="operator">&times;</button>
-                    <button className="number">4</button>
-                    <button className="number">5</button>
-                    <button className="number">6</button>
-                    <button className="operator">&minus;</button>
-                    <button className="number">1</button>
-                    <button className="number">2</button>
-                    <button className="number">3</button>
-                    <button className="operator">+</button>
-                    <button className="zero">0</button>
+
+                    <button className="number" onClick={handleNumber}>7</button>
+
+                    <button className="number" onClick={handleNumber}>8</button>
+
+                    <button className="number" onClick={handleNumber}>9</button>
+
+                    <button className="operator" onClick={handleOperator}>&times;</button>
+
+                    <button className="number" onClick={handleNumber}>4</button>
+                    
+                    <button className="number" onClick={handleNumber}>5</button>
+                    
+                    <button className="number" onClick={handleNumber}>6</button>
+                    
+                    <button className="operator" onClick={handleOperator}>&minus;</button>
+                    
+                    <button className="number" onClick={handleNumber}>1</button>
+                    
+                    <button className="number" onClick={handleNumber}>2</button>
+                    
+                    <button className="number" onClick={handleNumber}>3</button>
+                    
+                    <button className="operator" onClick={handleOperator}>+</button>
+                    
+                    <button className="zero" onClick={handleNumber}>0</button>
+                    
                     <button className="number">.</button>
+                    
+                    
                     <button className="operator">=</button>
                 </div>
 
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const container = document.getElementById('app');
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
